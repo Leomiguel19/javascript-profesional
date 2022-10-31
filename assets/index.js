82cea394c4dff28261c6bdb1f7c41e2dc10ba0ff
Leomiguel19 <leomiguel1907@gmail.com>
2022-10-31 09:34:43 -0400

    First plugin

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,14 @@
 import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from "./plugins/AutoPlay.js";
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({el: video});
+const player = new MediaPlayer({
+    el: video, 
+    plugins: [new AutoPlay()]
+});
 
-const button = document.querySelector('button');
+const button = document.querySelector("#playPause");
 button.onclick = () => player.togglePlay();
+
+const muteUnmute = document.querySelector('#unmuteMute');
+muteUnmute.onclick = () => player.unmuteMute();
